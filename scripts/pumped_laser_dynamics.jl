03d965407d2da46699dccb8785887ba8ba891b38
awage <>
2024-07-03 09:04:00 +0200

    Revert "split ring resonator"
    This reverts commit af9a467ade78b0beaa7a87996a9c7c2eb8c5eaa6.

--- scripts/pumped_laser_dynamics.jl
+++ scripts/pumped_laser_dynamics.jl
@@ -15,13 +15,12 @@ include(srcdir("print_fig.jl"))
 # doi:10.1016/j.physleta.2009.10.061
 
 function pumped_laser!(du, u, p, t)
-    L = 70; Tr = 8.7; r0 = 1.5e-4; σ12 = 3e-21; 
+    # α, β, γ, m0, m1 = p
+    L = 70; Tr = 8.7; r0 = 1.4e-4; σ12 = 3e-21; 
     τ = 1e-2; ξ1 = 2; ξ2= 0.4; N0 = 1.3e20; R = 0.8
     w0 = 3.5e-4; γ0 = 0.038; λg = 1.56e-4; β = 0.5
-    # α0 = N0*σ12 
-    α0 = 0.4
-    # αth = γ0 + 1/(2*L)*log(1/R)
-    αth = 3.92e-2
+    α0 = N0*σ12 
+    αth = γ0 + 1/(2*L)*log(1/R)
     # rw = 1 - exp(-2*(r0/w0)^2)
     rw = 0.308
     P,N = u
@@ -39,20 +38,25 @@ end
 
 function compute_basins_lasers(di::Dict)
     @unpack  res = di
+
     diffeq = (alg = Vern9(), reltol = 1e-10, maxiters = 1e9)
     ds = CoupledODEs(pumped_laser!, rand(2), []; diffeq)
-    yg = range(-1, 1; length = 10001)
-    xg = range(-1, 30; length = 10001)
+    yg = range(-5, 5; length = 10001)
+    xg = range(-30, 30; length = 10001)
     grid = (xg, yg)
-    mapper = AttractorsViaRecurrences(ds, grid)
-    yr = range(0, 1, length = res)
-    xr = range(0, 15, length = res)
+    mapper = AttractorsViaRecurrences(ds, grid; sparse = true, Δt = 0.01,   
+        mx_chk_hit_bas = 20000,
+        mx_chk_fnd_att = 100,
+        mx_chk_loc_att = 100, maximum_iterations = Int(1e8))
+    yr = range(-5, 5, length = res)
+    xr = range(-7, 7, length = res)
     bsn = @showprogress [ mapper([x,y]) for x in xr, y in yr]
     grid = (xr,yr)
     return @strdict(bsn, grid, res)
 end
 
-res = 100
+
+res = 200
 params = @strdict res
 print_fig(params, "basins_lasers", compute_basins_lasers; force = true)
 
